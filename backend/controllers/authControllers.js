72822c6fe41c33529222f719b622ec8e8f9511e2
mahak <kaushikikes@gmail.com>
2026-05-04 22:27:10 +0530

    login api and jwt token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,39 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+const loginuser = async(req,res)=>{
+    try{
+        const {email,password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid email"});
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid password"});
+        }
+
+        //create token
+        const token = jwt.sign({id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        );
+
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token,
+        });
+    }catch(err){
+        res.status(500).json({message:err.message});
+    }
+};
+
 
 const registeruser = async(req,res)=>{
     try{
@@ -10,7 +44,6 @@ const registeruser = async(req,res)=>{
         if(userExists){
             return res.status(400).json({message:"user already exist"});
         }
-
 
         //hash password
         const salt = await bcrypt.genSalt(10);
@@ -30,5 +63,5 @@ const registeruser = async(req,res)=>{
     }
 };
 
-module.exports = {registeruser};
+module.exports = {registeruser,loginuser};
 
