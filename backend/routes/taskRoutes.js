3009a481fd6ecb9336729b886e35d3dbc370fc94
mahak <kaushikikes@gmail.com>
2026-05-06 21:57:30 +0530

    update task and delete task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {createtask} = require('../controllers/taskController');
+const {createtask,gettask,updatetask,deletetask} = require('../controllers/taskController');
 const protect = require('../middleware/authmiddleware');
 
 router.post('/',protect,createtask);
+router.get("/", protect, gettask);
+router.put('/:id',protect, updatetask);
+router.delete('/:id',protect,deletetask);
 
 module.exports = router;
